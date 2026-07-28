760eb174024da425e1fe4f3a800a7d7f6adad7f3
DEVPRO\dario <dario.fernando.poveda@gmail.com>
2026-07-27 20:45:49 -0500

    Timeout

--- Src/Infrastructure/Persistence/Configuration/ConnectionConfig.cs
+++ Src/Infrastructure/Persistence/Configuration/ConnectionConfig.cs
@@ -29,7 +29,7 @@ public class ConnectionConfig
             $"Pooling=true;" +
             $"Min Pool Size=3;" +
             $"Max Pool Size=30;" +
-            $"Connection Timeout=120;" +
+            $"Connection Timeout=300;" +
             $"Application Name=LIB;" +
             $"Language=us_english;" +
             $"Encrypt=True;" +
